0c933c14180e1fbeff366d89c89db37ae9423737
cjg9040 <chl8273@kookmin.ac.kr>
2019-01-16 00:07:27 +0900

    chapter4

--- chapter4/server3.js
+++ chapter4/server3.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
-const parseCookies = (cookie = '')=>{
+const parseCookies = (cookie = '')=>
     cookie
-    .split
+    .split(';')
+    .map(v=>v.split('='))
+    .map(([k,...vs])=>[k,vs.join('=')])
+    .reduce((acc,[k,v])=>{
+        acc[k.trim()] = decodeURIComponent(v);
+        return acc;
+    },{});
 
-}
+http.createServer((req,res)=>{
+    const cookies = parseCookies(req.headers.cookie);
+    console.log(req.url,cookies);
+    res.writeHead(200,{'Set-Cookie':'mycookie=test'});
+    res.end('Hello Cooke');
+
+})
+    .listen(8082,()=>{
+        console.log('8082 port server on!');
+});
